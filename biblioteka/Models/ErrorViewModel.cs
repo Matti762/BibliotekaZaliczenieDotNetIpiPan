f45e328cef722a91db07581303a9623372928210
Matti <mateusz.szerszen@hotmail.com>
2019-04-28 14:22:12 +0200

    Dodaj pliki projektów.

---
+++ biblioteka/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace biblioteka.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
